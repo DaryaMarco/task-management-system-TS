545169f7689c7363d04b866bb123aa3a7c9e8201
Dar <Dar740141@gmail.com>
2026-07-30 11:20:49 +0330

    Use MongoDB service in GitHub Actions

--- server/src/tests/setup.ts
+++ server/src/tests/setup.ts
@@ -1,22 +1,32 @@
-import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+import mongoose  from "mongoose";
+import {MongoMemoryServer} from "mongodb-memory-server";
+
+let mongoServer : MongoMemoryServer;
 
 beforeAll(async () => {
-  await mongoose.connect(
-    process.env.MONGO_URI || "mongodb://localhost:27017/task-management-test"
-  );
+    mongoServer = await MongoMemoryServer.create();
+
+    const uri = mongoServer.getUri();
+
+    await mongoose.connect(uri);
+});
+
+afterEach(async ()=>{
+    const collections = mongoose.connection.collections;
+    for( const key in collections){
+        await collections[key].deleteMany({});
+    }
+
+});
+
+afterAll(async ()=>{
+    await mongoose.connection.close();
+    if(mongoServer){
+        await mongoServer.stop();
+    }
 });
 
 
-afterEach(async () => {
-  const collections = mongoose.connection.collections;
-
-  for (const key in collections) {
-    await collections[key].deleteMany({});
-  }
-});
-
-
-afterAll(async () => {
-  await mongoose.connection.dropDatabase();
-  await mongoose.connection.close();
-});
